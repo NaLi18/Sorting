bec831dac993bd9642dc09c850d64003f7c84d39
Na Li <nli@albany.edu>
2017-04-13 13:21:34 -0400

    Copy the Node class to the Number class and change the Node to the Number

--- Number.java
+++ Number.java
@@ -1,3 +1,71 @@
 public class Number
 {
+  String str;
+  Number next;
+  Number prev;
+  /*
+   * The constructor
+   * @param name
+   */
+  public Number(String name){
+    this.next = null;
+    this.prev = null;
+    this.str = name;
+  }
+  /*
+   * The non-argument constructor
+   */
+  public Number(){
+    this.str = null;
+    this.prev = null;
+    this.next = null;
+  }
+  /*
+   * The setNext method which set the nextNumber
+   * @param node
+   */
+  public void setNext(Number node){ 
+    this.next = node;
+  }
+  /*
+   * The getNext method return the next node
+   * @return 
+   */
+  public Number getNext(){
+    return this.next;
+  }
+  /*
+   * The setPrev method set the previou node
+   * @param node
+   */
+  public void setPrev(Number node){
+    this.prev = node;
+  }
+  /*
+   * The getPrev method return the previou node
+   * @return 
+   */
+  public Number getPrev(){
+    return this.prev;
+  }
+  /*
+   * The getValue method return the value of node
+   * @return 
+   */
+  public String getValue(){
+    return this.str;
+  }
+  /*
+   * The setValue method set the value of node
+   * @param x the string save in the node 
+   */
+  public void setValue(String x){
+    this.str = x;
+  }
+  /*
+   * The print method display the value of the node 
+   */
+  public void print(){
+    System.out.println(this.str + " ");
+  }
 }
